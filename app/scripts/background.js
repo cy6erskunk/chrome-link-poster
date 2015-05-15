5e6085ad3244760315e48e685f477330a0776753
Igor Shevchenko <cyberskunk@gmail.com>
2015-05-15 12:02:10 +0300

    add context menu item for links

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,4 +4,24 @@ chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-console.log('\'Allo \'Allo! Event Page');
+// The onClicked callback function.
+function onClickHandler(info, tab) {
+    console.log("item " + info.menuItemId + " was clicked");
+    console.log("URL: " + info.linkUrl);
+    // console.log("tab: " + JSON.stringify(tab));
+};
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+// Set up context menu tree at install time.
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    "title": "Test 'link' menu item",
+    "contexts": ["link"],
+    "id": "contextLink"
+  }, function() {
+    if (chrome.extension.lastError) {
+      console.log("Got error: " + chrome.extension.lastError.message);
+    }
+  });
+});
